ba7be09f5656679bcd4eade346a31a135f4d38f5
lyounga1 <lyounga01@gmail.com>
2025-12-09 15:44:54 +0000

    Deploying to gh-pages from @ lyounga1/lyounga1.github.io@2a8d019a920e7e91a9f405dc54b3f29af16b4ed9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -400,37 +400,7 @@ ninja.data = [{
           section: "News",},{id: "news-포트폴리오-웹사이트-1차-배포-완료-github-pages",
           title: '포트폴리오 웹사이트 1차 배포 완료 (GitHub Pages) 🚀',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{
+          section: "News",},{
         id: 'social-cv',
         title: 'CV',
         section: 'Socials',
